2f8dc9c2bcb8603929706fe306069147efb42759
KamelAbrahm <kamelabraham00@gmail.com>
2024-02-23 17:37:36 +0000

    WIP - Fetch Data
    API data rendering Only onSave

---
+++ src/components/API/screens/WeatherInformationScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const WeatherInformationScreen = () => {
+  return (
+    <View>
+      <Text>WeatherInformationScreen</Text>
+    </View>
+  );
+};
+
+export default WeatherInformationScreen;
+
+const styles = StyleSheet.create({});
