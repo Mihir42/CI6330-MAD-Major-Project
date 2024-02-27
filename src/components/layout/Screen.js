a6ab85178e6c467ef742cef8cb6a85d6f8051c6a
Mihir42 <124581857+Mihir42@users.noreply.github.com>
2024-02-27 20:56:19 +0000

    Refactoring first sprint (#4)
    Added a stack navigator, moved weather code into weather information screen, fixed state rendering issues concerning API fetch and fixed centring issues with content.

---
+++ src/components/layout/Screen.js
@@ -0,0 +1,27 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+
+export const Screen = ({ children }) => {
+  // Initialisation --------------------------
+  // State -----------------------------------
+  // Handlers --------------------------------
+  // View ------------------------------------
+  return (
+    <View style={styles.screen}>
+      {children}
+      <StatusBar style="light" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 15,
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export default Screen;
