a4f0d940416ffb8edefca0ce9a21f067b494a54e
KamelAbrahm <kamelabraham00@gmail.com>
2024-03-01 17:54:55 +0000

    StylingProgress
    Background colour,
    Share, Wind, Humidity, UV icons
    Text colour,
    Pending Refactoring
    
    Co-Authored-By: Mihir42 <124581857+Mihir42@users.noreply.github.com>

--- src/components/layout/Screen.js
+++ src/components/layout/Screen.js
@@ -1,5 +1,5 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
 
 export const Screen = ({ children }) => {
   // Initialisation --------------------------
@@ -18,9 +18,9 @@ const styles = StyleSheet.create({
   screen: {
     padding: 15,
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#005478',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 
--- src/components/screens/WeatherInformationScreen.js
+++ src/components/screens/WeatherInformationScreen.js
@@ -1,11 +1,11 @@
-import { StatusBar } from "expo-status-bar";
-import { useEffect, useState } from "react";
-import { Image, StyleSheet, Text, View, Button } from "react-native";
-import Screen from "../layout/Screen";
-import RenderCount from "../UI/RenderCount";
-import { Share } from "react-native";
-import API from "../API/API";
-
+import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
+import { Image, StyleSheet, Text, View, Button } from 'react-native';
+import Screen from '../layout/Screen';
+import RenderCount from '../UI/RenderCount';
+import { Share } from 'react-native';
+import API from '../API/API';
+import { Feather } from '@expo/vector-icons';
 const dummyWeather = {
   current: {
     cloud: 75,
@@ -16,7 +16,7 @@ const dummyWeather = {
     gust_mph: 9.6,
     humidity: 71,
     is_day: 0,
-    last_updated: "2024-02-27 18:45",
+    last_updated: '2024-02-27 18:45',
     last_updated_epoch: 1709059500,
     precip_in: 0,
     precip_mm: 0.02,
@@ -28,26 +28,26 @@ const dummyWeather = {
     vis_km: 10,
     vis_miles: 6,
     wind_degree: 230,
-    wind_dir: "SW",
+    wind_dir: 'SW',
     wind_kph: 9,
     wind_mph: 5.6,
   },
   location: {
-    country: "United Kingdom",
+    country: 'United Kingdom',
     lat: 51.52,
-    localtime: "2024-02-27 18:56",
+    localtime: '2024-02-27 18:56',
     localtime_epoch: 1709060178,
     lon: -0.11,
-    name: "Dummy London",
-    region: "City of London, Greater London",
-    tz_id: "Europe/London",
+    name: 'Dummy London',
+    region: 'City of London, Greater London',
+    tz_id: 'Europe/London',
   },
 };
 
 const WeatherInformationScreen = () => {
   // Intialisation ------------------------------------
   const weatherEndpoint =
-    "http://api.weatherapi.com/v1/current.json?key=8c36386ccc784250871133222242102&q=London&aqi=no";
+    'http://api.weatherapi.com/v1/current.json?key=8c36386ccc784250871133222242102&q=London&aqi=no';
 
   // State --------------------------------------------
   const [weather, setWeather] = useState(dummyWeather);
@@ -75,25 +75,43 @@ const WeatherInformationScreen = () => {
     try {
       const shareResponse = await Share.share(shareOptions);
     } catch (error) {
-      console.log("Error -> ", error);
+      console.log('Error -> ', error);
     }
   };
 
   return (
     <Screen>
+      <Feather
+        style={styles.containerTop}
+        name="share"
+        size={28}
+        color="white"
+        onPress={handleWeather}
+      />
       <Image
         style={styles.imageIcon}
         source={{ uri: `https:${weather.current.condition.icon}` }}
       />
-      <Text style={styles.textHeader}>{weather.location.name}</Text>
+      <Text style={styles.textHeaderTemp}>{weather.current.temp_c}°C</Text>
       <Text style={styles.textHeader}>
-        {weather.current.condition.text} {weather.current.temp_c}°C
+        {weather.location.name}, {'\n'}
+        {weather.current.condition.text}
       </Text>
-      <Text style={styles.textNormal}>
-        Wind(kph): {weather.current.wind_kph}, Humidity:{" "}
-        {weather.current.humidity}% UV: {weather.current.uv}
-      </Text>
-      <Button onPress={handleWeather} title="Share"></Button>
+      <View style={styles.containerBottom}>
+        <Text style={styles.textNormal}>
+          <Feather name="droplet" size={40} color="white" /> {'\n'}
+          Humidity: {weather.current.humidity}%
+        </Text>
+        <Text style={styles.textNormal}>
+          <Feather name="wind" size={40} color="white" /> {'\n'}
+          Wind: {weather.current.wind_kph}KPH
+        </Text>
+        <Text style={styles.textNormal}>
+          <Feather name="sun" size={40} color="white" /> {'\n'}
+          UV Index: {'\n'}
+          {weather.current.uv}
+        </Text>
+      </View>
     </Screen>
   );
 };
@@ -103,16 +121,41 @@ export default WeatherInformationScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: '#fff',
   },
-  viewCenter: { padding: 30, marginBottom: 200, alignContent: "center" },
-  viewBottom: {},
+  containerTop: {
+    alignSelf: 'flex-start',
+    verticalAlign: 'top',
+    flex: 1,
+  },
+  containerBottom: {
+    flex: 1,
+    marginBottom: 100,
+    marginTop: 50,
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+  },
   textHeader: {
-    fontWeight: "bold",
+    fontWeight: 'bold',
     fontSize: 30,
+    color: 'white',
+    textAlign: 'center',
   },
-  textNormal: { fontSize: 20, textAlign: "center" },
+  textHeaderTemp: {
+    fontWeight: 'bold',
+    fontSize: 44,
+    color: 'white',
+    textAlign: 'center',
+  },
+  textNormal: {
+    flex: 1,
+    fontSize: 20,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    color: 'white',
+  },
   imageIcon: {
+    marginTop: 50,
     width: 120,
     height: 120,
   },
