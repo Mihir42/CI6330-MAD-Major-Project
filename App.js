41e29a95869603d2b5978af9d62bf4aef221874c
Mihir42 <124581857+Mihir42@users.noreply.github.com>
2024-03-15 16:05:11 +0000

    Tab navigation added (#12)
    Allows navigation between two screens implemented with tabs
    
    Co-authored-by: KamelAbrahm <153553458+KamelAbrahm@users.noreply.github.com>

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import WeatherInformationScreen from "./src/components/screens/WeatherInformationScreen";
 import { StyleSheet } from "react-native";
 import WeatherPostsScreen from "./src/components/screens/WeatherPostsScreen";
@@ -7,13 +8,14 @@ import WeatherPostsScreen from "./src/components/screens/WeatherPostsScreen";
 export default function App() {
   // Intialisation ------------------------------------
   const stack = createNativeStackNavigator();
+  const Tab = createBottomTabNavigator();
   // View ---------------------------------------------
   return (
     <NavigationContainer>
-      <stack.Navigator initialRouteName="Posts">
-        <stack.Screen name="Weather" component={WeatherInformationScreen} />
-        <stack.Screen name="Posts" component={WeatherPostsScreen} />
-      </stack.Navigator>
+      <Tab.Navigator initialRouteName="Posts">
+        <Tab.Screen name="Weather" component={WeatherInformationScreen} />
+        <Tab.Screen name="Posts" component={WeatherPostsScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
