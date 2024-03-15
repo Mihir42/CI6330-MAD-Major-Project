141cc9e7a518144260648d2c741fbe00fde19d27
Mihir42 <124581857+Mihir42@users.noreply.github.com>
2024-03-15 15:52:11 +0000

    Able to see existing posts (#11)
    Posts show image and title
    
    Co-authored-by: KamelAbrahm <153553458+KamelAbrahm@users.noreply.github.com>

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import WeatherInformationScreen from "./src/components/screens/WeatherInformationScreen";
 import { StyleSheet } from "react-native";
+import WeatherPostsScreen from "./src/components/screens/WeatherPostsScreen";
 
 export default function App() {
   // Intialisation ------------------------------------
@@ -9,8 +10,9 @@ export default function App() {
   // View ---------------------------------------------
   return (
     <NavigationContainer>
-      <stack.Navigator>
+      <stack.Navigator initialRouteName="Posts">
         <stack.Screen name="Weather" component={WeatherInformationScreen} />
+        <stack.Screen name="Posts" component={WeatherPostsScreen} />
       </stack.Navigator>
     </NavigationContainer>
   );
