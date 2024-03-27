d1de230980ba257affc895b16e2dd427c9dbf26c
KamelAbrahm <153553458+KamelAbrahm@users.noreply.github.com>
2024-03-27 21:34:58 +0000

    Added gps location but global state issue (#20)
    Co-authored-by: Mihir42 <124581857+Mihir42@users.noreply.github.com>

---
+++ src/components/entity/posts/Geo.js
@@ -0,0 +1,61 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect, createContext } from 'react';
+import * as Location from 'expo-location';
+
+const dummyLocation = {
+  coords: {
+    accuracy: 100,
+    altitude: 61.900001525878906,
+    altitudeAccuracy: 100,
+    heading: 0,
+    latitude: 52.2371,
+    longitude: 0.8944,
+    speed: 0,
+  },
+  mocked: false,
+  timestamp: 1711571170839,
+};
+
+const Geo = () => {
+  const [location, setLocation] = useState(dummyLocation);
+  const [address, setAddress] = useState('Balls');
+  const [city, setCity] = useState('Balls');
+
+  let nuts = 'Nut';
+  const LocationContext = createContext();
+
+  const resverseGeocode = async () => {
+    const reverseGeocodedAddress = await Location.reverseGeocodeAsync({
+      longitude: location.coords.longitude,
+      latitude: location.coords.latitude,
+    });
+    let tempAddress = '';
+    tempAddress = reverseGeocodedAddress[0].subregion;
+    setCity(tempAddress);
+  };
+
+  useEffect(() => {
+    const getPermissions = async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        console.log('Please grant location permissions');
+        return;
+      }
+
+      let currentLocation = await Location.getCurrentPositionAsync({});
+      setLocation(currentLocation);
+      resverseGeocode();
+    };
+    getPermissions();
+  }, [city]);
+
+  return (
+    <View>
+      <Text>{city}</Text>
+    </View>
+  );
+};
+
+export default Geo;
+
+const styles = StyleSheet.create({});
